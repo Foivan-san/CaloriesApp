d1988707ae0afcf4d5c5f28d46a0e607e5e5b89b
ΕΥΣΤΡΑΤΙΟΣ ΚΑΡΑΜΠΑΣΗΣ <ice18390169@uniwa.gr>
2022-06-17 13:26:22 +0300

    ekana push siuuu

--- app/src/main/java/com/example/firstandroidapp/DrawerActivity.java
+++ app/src/main/java/com/example/firstandroidapp/DrawerActivity.java
@@ -52,7 +52,7 @@ public class DrawerActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.drawer, menu);
+        getMenuInflater().inflate(R.menu.activity_main_drawer, menu);
         return true;
     }
 
