af7b49847516e5537d1aa786a8b78f0019f4a1ec
ΕΥΣΤΡΑΤΙΟΣ ΚΑΡΑΜΠΑΣΗΣ <ice18390169@uniwa.gr>
2022-06-26 12:58:11 +0300

    check check

--- app/src/main/java/com/example/firstandroidapp/FoodActivity.java
+++ app/src/main/java/com/example/firstandroidapp/FoodActivity.java
@@ -3,38 +3,43 @@ package com.example.firstandroidapp;
 
 import android.database.Cursor;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 
 public class FoodActivity extends AppCompatActivity {
 
+
     DBAdapter dbAdapter;
-    Button add_food_btn;
-    EditText add_food;
-
     ListView food_list;
-
     ArrayList<String> listItem;
     ArrayAdapter<String> adapter;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food);
 
+        /*
+        food_list = findViewById(R.id.food_list);
+
+        adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_multiple_choice, listItem);
+        food_list.setAdapter(adapter);
+         */
+
         dbAdapter = new DBAdapter(this);
 
         listItem = new ArrayList<String>();
-
-        add_food_btn = findViewById(R.id.add_food_btn);
-        add_food = findViewById(R.id.add_food);
         food_list = findViewById(R.id.food_list);
 
         viewData();
@@ -44,16 +49,7 @@ public class FoodActivity extends AppCompatActivity {
             Toast.makeText(FoodActivity.this, ""+text, Toast.LENGTH_SHORT).show();
         });
 
-        add_food_btn.setOnClickListener(v -> {
-            String foodItem = add_food.getText().toString();
-            if(!foodItem.equals("") && dbAdapter.insertDataFood(foodItem)){
-                Toast.makeText(FoodActivity.this, "Food added", Toast.LENGTH_SHORT).show();
-                add_food.setText("");
-            }
-            else{
-                Toast.makeText(FoodActivity.this, "Food not added", Toast.LENGTH_SHORT).show();
-            }
-        });
+
     }
 
     private void viewData() {
@@ -66,10 +62,31 @@ public class FoodActivity extends AppCompatActivity {
                 listItem.add(cursor.getString( 1));
             }
 
-            adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listItem);
+            adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_multiple_choice, listItem);
             food_list.setAdapter(adapter);
         }
     }
 
 
+    @Override
+    public boolean onCreateOptionsMenu(@NonNull Menu menu) {
+        getMenuInflater().inflate(R.menu.done_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item){
+        int id = item.getItemId();
+        if (id == R.id.item_done){
+            String itemSelected = "Selected items: \n";
+            for (int i = 0; i <food_list.getCount(); i++){
+                if (food_list.isItemChecked(i)){
+                    itemSelected += food_list.getItemAtPosition(i) + "\n";
+                }
+            }
+            Toast.makeText(this,itemSelected,Toast.LENGTH_LONG).show();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
--- app/src/main/java/com/example/firstandroidapp/WelcomeActivity.java
+++ app/src/main/java/com/example/firstandroidapp/WelcomeActivity.java
@@ -1,23 +1,20 @@
 package com.example.firstandroidapp;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.lifecycle.MediatorLiveData;
-
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 
 import com.google.android.material.navigation.NavigationView;
-import com.google.firebase.firestore.auth.User;
 
 public class WelcomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
